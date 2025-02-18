35bf051f53155685e02d458d599b192ad3f866e4
chandra865 <shekhar777229@gmail.com>
2025-02-18 13:19:00 +0530

    Database connection and server start

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import {config} from "dotenv";
+config({
+    path : "./.env"   
+})
+
+
+import connectDB from "./db/connection.js";
+import {app} from './app.js'
+
+//Database connection and server start
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+})
+.catch((error) =>{
+    console.log("MongoDb Connnection failed !!!", err);
+})
